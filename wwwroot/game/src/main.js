5a096d79fa7a53aec5855b34a26ddefaf36a8456
caglarkabaca <cacarekt@yandex.com>
2024-09-06 17:21:09 +0300

    blurry wheel fix

--- wwwroot/game/src/main.js
+++ wwwroot/game/src/main.js
@@ -1,4 +1,4 @@
-import { Game } from 'phaser';
+import {Game} from 'phaser';
 
 import {GameScene} from './scenes/GameScene.js'
 import {WinnerScene} from './scenes/WinnerScene.js'
@@ -6,7 +6,7 @@ import {PreloadScene} from './scenes/PreloadScene.js';
 
 //  Find out more information about the Game Config at: https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
 const config = {
-    type: Phaser.AUTO,
+    type: Phaser.WebGl,
     width: 400,
     height: 600,
     parent: 'game-container',
--- wwwroot/game/src/scenes/GameScene.js
+++ wwwroot/game/src/scenes/GameScene.js
@@ -8,7 +8,6 @@ const colors = [0x4724A1, 0xB387E8, 0x6B1CD5, 0x4D2379];
 
 const TEXT_COLOR = '#3c005a';
 
-
 class GameScene extends Phaser.Scene {
 
     constructor() {
@@ -36,8 +35,12 @@ class GameScene extends Phaser.Scene {
     }
 
     create() {
-        this.RoundedTextBox(200, 475, 50)
-        this.SpinWheel(225)
+        let {width, height} = this.sys.game.canvas;
+        this.ratio = width / 400;
+
+        this.RoundedTextBox(200 * this.ratio, 475 * this.ratio, 50 * this.ratio)
+        this.SpinWheel(225 * this.ratio)
+
     }
 
     spin() {
@@ -46,9 +49,9 @@ class GameScene extends Phaser.Scene {
 
         this.input.off("pointerdown", this.spin, this)
 
-        const particles = this.add.particles(200, 33.5, 'red', {
+        const particles = this.add.particles(200 * this.ratio, 33.5 * this.ratio, 'red', {
             speed: 100,
-            scale: { start: 1, end: 0 },
+            scale: {start: 1, end: 0},
             blendMode: 'ADD'
         });
         particles.depth = 10
@@ -62,8 +65,8 @@ class GameScene extends Phaser.Scene {
         this.tweens.add({
 
             targets: [this.wheel, this.wheeldots],
-            angle: - (360 * rounds + spinAngle),
-            duration: 2000,
+            angle: -(360 * rounds + spinAngle),
+            duration: 3500,
             ease: "Cubic.easeOut",
             callbackScope: this,
 
@@ -94,11 +97,11 @@ class GameScene extends Phaser.Scene {
             add: false
         });
         graphics.fillStyle(0xFFFFFF, 1);
-        graphics.fillCircle(182, 182, 182);
-        graphics.generateTexture("wheelbg", 182 * 2, 182 * 2);
-        const bg = this.add.sprite(200, h, "wheelbg")
+        graphics.fillCircle(182 * this.ratio, 182 * this.ratio, 182 * this.ratio);
+        graphics.generateTexture("wheelbg", 182 * 2 * this.ratio, 182 * 2 * this.ratio);
+        const bg = this.add.sprite(200 * this.ratio, h, "wheelbg")
         bg.depth = -2
-        
+
         // UCGEN
         var graphics = this.make.graphics({
             x: 0,
@@ -106,11 +109,11 @@ class GameScene extends Phaser.Scene {
             add: false
         });
         graphics.fillStyle(0xFFFFFF, 1);
-        graphics.fillTriangle(165, 0, 195, 0, 180, 40)
-        graphics.generateTexture("wheelbgTriangle", 180 * 2, 180 * 2);
-        const bgTriangle = this.add.sprite(200, h - 20, "wheelbgTriangle")
+        graphics.fillTriangle(165 * this.ratio, 0, 195 * this.ratio, 0, 180 * this.ratio, 40 * this.ratio)
+        graphics.generateTexture("wheelbgTriangle", 180 * 2 * this.ratio, 180 * 2 * this.ratio);
+        const bgTriangle = this.add.sprite(200 * this.ratio, h - 20 * this.ratio, "wheelbgTriangle")
         bgTriangle.depth = 1
-        
+
         // NOKTA
         var graphics = this.make.graphics({
             x: 0,
@@ -118,9 +121,9 @@ class GameScene extends Phaser.Scene {
             add: false
         });
         graphics.fillStyle(0xFFFFFF, 1);
-        graphics.fillCircle(180, 180, 25)
-        graphics.generateTexture("wheelbgPoint", 180 * 2, 180 * 2);
-        const bgPoint = this.add.sprite(200, h, "wheelbgPoint")
+        graphics.fillCircle(180 * this.ratio, 180 * this.ratio, 25 * this.ratio)
+        graphics.generateTexture("wheelbgPoint", 180 * 2 * this.ratio, 180 * 2 * this.ratio);
+        const bgPoint = this.add.sprite(200 * this.ratio, h, "wheelbgPoint")
 
         const parts = []
         const angle = 360 / this.datas.length;
@@ -136,19 +139,19 @@ class GameScene extends Phaser.Scene {
             // setting graphics fill style
             graphics.fillStyle(colors[i % 4], 1);
             // drawing the slice
-            graphics.slice(175, 175, 175, Phaser.Math.DegToRad(i * angle), Phaser.Math.DegToRad((i + 1) * angle), false);
+            graphics.slice(175 * this.ratio, 175 * this.ratio, 175 * this.ratio, Phaser.Math.DegToRad(i * angle), Phaser.Math.DegToRad((i + 1) * angle), false);
             // filling the slice
             graphics.fillPath();
-            
-            graphics.generateTexture("label" + i, 175 * 2, 175 * 2);
+
+            graphics.generateTexture("label" + i, 175 * 2 * this.ratio, 175 * 2 * this.ratio);
             const _part = this.add.sprite(0, 0, "label" + i)
             _part.setOrigin(0.5, 0.5)
             _part.depth = -1
 
             const radianAngle = Phaser.Math.DegToRad(angle * i + angle / 2);
 
-            const text = this.add.text(175 * 0.95 * Math.cos(radianAngle), 175 * 0.95 * Math.sin(radianAngle), this.datas[i].title, {
-                fontSize: 18,
+            const text = this.add.text(175 * 0.95 * Math.cos(radianAngle) * this.ratio, 175 * 0.95 * Math.sin(radianAngle) * this.ratio, this.datas[i].title, {
+                fontSize: 56,
                 color: "#fff",
                 fontFamily: "Montserrat",
                 resolution: 2
@@ -158,9 +161,9 @@ class GameScene extends Phaser.Scene {
             text.angle = angle * i + angle / 2
             parts.push(this.add.container(0, 0, [_part, text]))
         }
-        this.wheel = this.add.container(200, h, parts);
+        this.wheel = this.add.container(200 * this.ratio, h, parts);
         this.wheel.depth = -2
-        
+
         // çember üzerindeki noktalar
         var graphics = this.make.graphics({
             x: 0,
@@ -168,19 +171,19 @@ class GameScene extends Phaser.Scene {
         });
         graphics.fillStyle(0xFFFFFF, 1)
         for (var i = 0; i < this.datas.length; i++) {
-            graphics.fillCircle(175 + Math.cos(angle * i * Math.PI / 180) * 175, 175 + Math.sin(angle * i *  Math.PI / 180) * 175, 7)
+            graphics.fillCircle((175 + Math.cos(angle * i * Math.PI / 180) * 175) * this.ratio, (175 + Math.sin(angle * i * Math.PI / 180) * 175) * this.ratio, 7 * this.ratio)
         }
-        graphics.generateTexture("dot" + i, 175 * 2, 175 * 2);
-        this.wheeldots = this.add.sprite(200, h, "dot" + i)
-        
+        graphics.generateTexture("dot" + i, 175 * 2 * this.ratio, 175 * 2 * this.ratio);
+        this.wheeldots = this.add.sprite(200 * this.ratio, h, "dot" + i)
+
         this.wheel.angle = angle / 3
         this.wheeldots.angle = angle / 3
-        
+
     }
 
     RoundedTextBox(x, y, padding) {
         const alttext = this.add.text(x, y, this.startButtonText, {
-            fontSize: '28px',
+            fontSize: '72px',
             color: "#ffffff",
             fontFamily: "Montserrat",
             resolution: 2
@@ -200,18 +203,17 @@ class GameScene extends Phaser.Scene {
 
     ShowResult(winner) {
 
-        this.ResultBg(50, 50 + 75, 300, 250, 45)
-        this.SuccessText(200, 50 + 75, this.successText
+        this.ResultBg(50 * this.ratio, (50 + 75) * this.ratio, 300 * this.ratio, 250 * this.ratio, 45 * this.ratio)
+        this.SuccessText(200 * this.ratio, (50 + 75) * this.ratio, this.successText
             .replace('{title}', winner.title)
             .replace('{code}', winner.code)
             .replace('{description}', winner.description)
             .split('\\n'), winner.code
         )
-        this.TextBox(200, 250 + 75, 50, this.endButtonText, () => {
+        this.TextBox(200 * this.ratio, (250 + 75) * this.ratio, 50 * this.ratio, this.endButtonText, () => {
             if (winner.customRedirectUrl != null) {
                 window.location.replace(winner.customRedirectUrl)
-            }
-            else {
+            } else {
                 window.location.replace(winner.redirectUrl)
             }
         });
@@ -229,18 +231,18 @@ class GameScene extends Phaser.Scene {
             y: y - ((padding - 5) / 2)
         });
         graphics.fillStyle(0x000000, 0.90);
-        graphics.fillRoundedRect(0, 0, w + padding - 5, h + padding - 5, 10);
+        graphics.fillRoundedRect(0, 0, w + padding - 5, h + padding - 5, 10 * this.ratio);
     }
 
     TextBox(textX, textY, padding, endText, onClick) {
         // End Button
         var buttonText = this.add.text(textX, textY, endText, {
-            fontSize: '24px',
+            fontSize: '72px',
             fill: '#ffffff',
             align: "center",
             fontFamily: "Montserrat",
             resolution: 2,
-            wordWrap: { width: 250 }
+            wordWrap: {width: 250 * this.ratio}
         });
         buttonText.setOrigin(0.5, 0.5)
         buttonText.setDepth(1)
@@ -252,8 +254,8 @@ class GameScene extends Phaser.Scene {
             y: textBounds.y - (padding / 2)
         });
         buttonGraphics.fillStyle(0x3c005a, 1);
-        buttonGraphics.fillRoundedRect(0, 0, textBounds.width + padding, textBounds.height + padding, 12);
-        
+        buttonGraphics.fillRoundedRect(0, 0, textBounds.width + padding, textBounds.height + padding, 12 * this.ratio);
+
         buttonGraphics.setInteractive(new Phaser.Geom.Rectangle(0, 0, textBounds.width + padding, textBounds.height + padding), Phaser.Geom.Rectangle.Contains);
         buttonGraphics.on("pointerdown", onClick)
     }
@@ -261,28 +263,28 @@ class GameScene extends Phaser.Scene {
     SuccessText(x, y, successText, code) {
         var width = 0;
         const infoText = this.add.text(x, y, code, {
-            fontSize: 52,
+            fontSize: 88,
             color: "#fff",
             fontFamily: 'Montserrat',
             align: "center",
-            wordWrap: { width: 285 },
+            wordWrap: {width: 285 * this.ratio},
             resolution: 2
         });
         infoText.depth = 3
         infoText.setOrigin(0.5, 0);
         successText.forEach((text, index) => {
-            const infoText = this.add.text(x, y + 75, text, {
-                fontSize: 12,
+            const infoText = this.add.text(x, y + 50 * this.ratio, text, {
+                fontSize: 48,
                 color: "#fff",
                 fontFamily: 'Montserrat',
                 align: "center",
-                wordWrap: { width: 285 },
+                wordWrap: {width: 285 * this.ratio},
                 resolution: 2
             });
             infoText.depth = 3
             infoText.setOrigin(0.5, 0);
             y += infoText.height + 5
-            
+
         });
     }
 }
